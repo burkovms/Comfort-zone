e6311da8e2791b0299430bb658b625fbe5742f31
burkovms <emigrant911@gmail.com>
2025-06-19 22:21:18 +0300

    test commit

--- src/app/programs/[slug]/page.tsx
+++ src/app/programs/[slug]/page.tsx
@@ -1,36 +1,52 @@
+// import { notFound } from 'next/navigation';
+// import { programCategories } from '@/data/programs';
+// import './ProgramDetail.scss';
+
+// const findProgramBySlug = (slug: string) =>
+//   programCategories.flatMap(category => category.programs).find(program => program.slug === slug);
+
+// export async function generateMetadata({ params }: { params: { slug: string } }) {
+//   const { slug } = await params;
+//   const program = findProgramBySlug(slug);
+
+//   if (!program) return {};
+
+//   return {
+//     title: program.title,
+//     description: program.description,
+//   };
+// }
+
+// export async function generateStaticParams() {
+//   return programCategories
+//     .flatMap(category => category.programs)
+//     .map(tariff => ({
+//       slug: tariff.slug,
+//     }));
+// }
+
+// export default async function ProgramDetail({ params }: { params: { slug: string } }) {
+//   const { slug } = await params;
+//   const program = findProgramBySlug(slug);
+
+//   if (!program) return notFound();
+
+//   return (
+//     <div className="page-container p-block">
+//       <h1>{program.title}</h1>
+//     </div>
+//   );
+// }
 import { notFound } from 'next/navigation';
 import { programCategories } from '@/data/programs';
-import './ProgramDetail.scss';
 
-const findProgramBySlug = (slug: string) =>
-  programCategories.flatMap(category => category.programs).find(program => program.slug === slug);
-
-export async function generateMetadata({ params }: { params: { slug: string } }) {
+export default async function ProgramDetail({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  const program = findProgramBySlug(slug);
-
-  if (!program) return {};
-
-  return {
-    title: program.title,
-    description: program.description,
-  };
-}
-
-export async function generateStaticParams() {
-  return programCategories
+  const program = programCategories
     .flatMap(category => category.programs)
-    .map(tariff => ({
-      slug: tariff.slug,
-    }));
-}
-
-export default async function TariffDetail({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
-  const program = findProgramBySlug(slug);
+    .find(program => program.slug === slug);
 
   if (!program) return notFound();
-
   return (
     <div className="page-container p-block">
       <h1>{program.title}</h1>
